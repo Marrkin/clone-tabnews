bab12a37c64f2db3140d9eedd8a19951729eeec0
Marrkin <56325780+Marrkin@users.noreply.github.com>
2025-02-09 20:39:52 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+//TODO: TEMOS O DESAFIO DE IMPACTAR ALGUEM.
+
+function Home(){
+    return <h1>Isso definitivamente não é uma mensagem de teste!</h1>
+}
+
+
+export default Home //Home e o nosso componente padrao, e a partir dele chamaremos outras funcoes
