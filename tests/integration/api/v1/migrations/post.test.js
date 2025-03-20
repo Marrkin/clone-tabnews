e029afe9fffa5bb7fd8cdceeb4ac210e1186b141
Marrkin <56325780+Marrkin@users.noreply.github.com>
2025-03-20 23:09:00 +0000

    Corrige a variavel response nos testes

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,5 +1,5 @@
 test("POST to api/v1/migrations should return 200", async () => {
-  response = await fetch("http://localhost:3000/api/v1/migrations", {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
   expect(response.status).toBe(200);
