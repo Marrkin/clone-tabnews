40bc406f1f8f9a4799886203a27bba66858c40a2
Marrkin <4050lek@gmail.com>
2025-03-22 16:26:57 -0300

    Finaliza o endpoint `/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,31 +1,36 @@
-import { up } from "infra/migrations/1742140436096_test-migration";
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationsOptions = {
+    dbClient: dbClient,
+    dir: join("infra", "migrations"),
+    migrationsTable: "pgmigrations",
+    direction: "up",
+    dryRun: true,
+    verbose: true,
+  };
+
   if (request.method === "GET") {
     //Instanciar
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dir: join("infra", "migrations"),
-      migrationsTable: "pgmigrations",
-      direction: "up",
-      dryRun: true,
-      verbose: true,
-    });
-    return response.status(200).json(migrations); //Aqui é o nosso retorno à VIEW
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations); //Aqui é o nosso retorno à VIEW
   }
 
   if (request.method === "POST") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dir: join("infra", "migrations"),
-      migrationsTable: "pgmigrations",
-      direction: "up",
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
       dryRun: false,
-      verbose: true,
     });
-    return response.status(200).json(migrations);
+
+    await dbClient.end();
+    return migratedMigrations.length == 0
+      ? response.status(200).json(migratedMigrations)
+      : response.status(201).json(migratedMigrations);
   }
 
   return response.status(405).end(); //Method not Allowed
