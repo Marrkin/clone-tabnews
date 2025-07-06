dd752607315bae27c56bf405e37b828edadc90f0
Marcos Garcia <56325780+Marrkin@users.noreply.github.com>
2025-07-06 21:12:42 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,23 +4,27 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json(); //Aqui estamos pegando a resposta HTTP e Parseando para json para que usemos como um objeto js
+      const responseBody = await response.json(); //Aqui estamos pegando a resposta HTTP e Parseando para json para usar como um objeto js
 
-  //UpdatedAt
-  expect(responseBody.updated_at).toBeDefined();
-  const parsedDate = new Date(responseBody.updated_at).toISOString(); //Certifica de que realmente é um objeto Date
-  expect(responseBody.updated_at).toEqual(parsedDate);
+      //UpdatedAt
+      expect(responseBody.updated_at).toBeDefined();
+      const parsedDate = new Date(responseBody.updated_at).toISOString(); //Certifica de que realmente é um objeto Date
+      expect(responseBody.updated_at).toEqual(parsedDate);
 
-  //DatabaseVersion
-  expect(responseBody.dependencies.database.version).toBe("16.0");
+      //DatabaseVersion
+      expect(responseBody.dependencies.database.version).toBe("16.0");
 
-  //maxConnections
-  expect(responseBody.dependencies.database.max_connections).toBe(100);
+      //maxConnections
+      expect(responseBody.dependencies.database.max_connections).toBe(100);
 
-  //activeConnections
-  expect(responseBody.dependencies.database.opened_connections).toBe(1);
+      //activeConnections
+      expect(responseBody.dependencies.database.opened_connections).toBe(1);
+    });
+  });
 });
